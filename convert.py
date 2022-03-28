d9fddfb2879757bb2fc14a105fc147515879c5ec
Loginov Alexey <48813221+Fallern@users.noreply.github.com>
2022-03-28 04:04:50 +0300

    feat: implement a request to the cloudconvert API

---
+++ convert.py
@@ -0,0 +1,42 @@
+import os
+import cloudconvert
+import urllib.request
+import asyncio
+from config import CLOUDCONVERT_TOKEN
+
+cloudconvert.configure(api_key=CLOUDCONVERT_TOKEN,
+                       sandbox=True)
+
+
+# создание работы для конвертации одного формата в другой с использование API и преобразуем полученный код с сайта
+def convert_BGC(number_college):
+    filename = f'zamena{number_college}k.xlsx'
+    path = f'sheduleXLSX/{filename}'
+    data = cloudconvert.Job.create(payload={
+        "tasks": {
+            'upload_shedule_xlsx': {
+                'operation': 'import/upload'
+            },
+            'xlsx_to_png': {
+                'operation': 'convert',
+                'input': 'upload_shedule_xlsx',
+                'output_format': 'png',
+                'filename': f'download_{filename[:-5]}.png'  # удаляем xlsx расширение с помощью среза
+            },
+            'export_png': {
+                'operation': 'export/url',
+                'input': 'xlsx_to_png'
+            }
+        }
+    })
+    upload_task_id = data['tasks'][0]['id']
+    upload_task = cloudconvert.Task.find(id=upload_task_id)
+    task = cloudconvert.Task.upload(file_name=path, task=upload_task)
+
+    exported_task_id = data.get('tasks')[2].get('id')
+    res = cloudconvert.Task.wait(id=exported_task_id)  # Wait for job completion
+    file = res.get("result").get("files")[0]
+    filename = file['filename']
+    file_url = file['url']
+    cloudconvert.download(filename=filename, url=file_url)
+    os.replace(filename, f'college_building_img/download_zamena{number_college}.png')
