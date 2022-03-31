5d7cb36481e0aabab73a1b2435a10dea461ac14a
Fallern <alesha.loginov2000@gmail.com>
2022-03-31 19:17:19 +0300

    refactor: change message answer to the user

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,7 +1,13 @@
 from aiogram import types
 from loader import dp
+from keyboards.default.keyboard_menu import kb_menu
 
 
 @dp.message_handler(text='/start')
 async def command_start(message: types.Message):
-    await message.answer(f"Привет{message.from_user.full_name}")
+    sticker = 'CAACAgIAAxkBAAEEVBdiRY_oY-7Vds-Fr1TzW8TIv1x2xwACFgADrscyCt5fUMuqSVWmIwQ'
+    await message.answer_sticker(sticker=sticker)
+    await message.answer(
+        f"Привет, <b>{message.chat.full_name}</b>, Я - очень простой бот, которой поможет тебе очень просто получать"
+        f" расписание занятий по корпусам.\n\n"
+        f"Выбери команду для продолжения или напиши /help")
