9d21b4608d6444864482daa06c290263f6e055c4
Fallern <alesha.loginov2000@gmail.com>
2022-04-03 17:00:24 +0300

    refactor: add on_shutdown and upload shedule files on start

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
+import asyncio
+import logging
+
 from aiogram import executor
 
 import handlers
+from database import connect
 from loader import dp
-from utils.notify_admins import on_startup_notify
-from utils.set_bot_commands import set_default_commands
+from utils.service.update_logic import load_on_start_all_files_xlsx
+from utils.service.update_logic import check_shedule
 
 
 async def on_startup(dispatcher):
-    await on_startup_notify(dispatcher)
+    logging.basicConfig(level=logging.INFO)
+    await connect.database_init()
+    await load_on_start_all_files_xlsx()
+    asyncio.create_task(check_shedule())
+    # await convert.start_load_all_files_png()
 
-    await set_default_commands(dispatcher)
+
+async def on_shutdown(dispatcher):
+    await connect.database_close()
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
