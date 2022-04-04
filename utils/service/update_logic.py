120f61dcc82ecfc0e09682790af6a5c45cdbe236
Fallern <alesha.loginov2000@gmail.com>
2022-04-04 16:19:18 +0300

    refactor: add logging.info

--- utils/service/update_logic.py
+++ utils/service/update_logic.py
@@ -24,13 +24,14 @@ async def load_on_start_all_files_xlsx():
             file_xlsx.write(file.content)
         timetable_old_size[f'timetable_old_size{college_number}'] = os.path.getsize(
             f'media/sheduleXLSX/zamena{college_number}k.xlsx')
+        logging.info(f'load xlsx file college {college_number}')
         await save_img_groups_to_file(f'media/sheduleXLSX/zamena{college_number}k.xlsx', college_number)
 
 
 async def comparison_size_bgc(college_number: int, timetable_size: dict) -> None:
     """Проверяем размер исходного(старого файла с расписанием)
      с новым, если они не равны - отсылаем запрос"""
-    logging.info('start check size bgc shedule file')
+    logging.info(f'start check size bgc shedule file college {college_number}')
     url = f'http://www.bgtc.su/wp-content/uploads/raspisanie/zamena{college_number}k.xlsx'
     timetable_new_size = int(urllib.request.urlopen(url).info()['Content-Length'])
     if timetable_new_size != timetable_size[f'timetable_old_size{college_number}']:
@@ -44,11 +45,11 @@ async def comparison_size_bgc(college_number: int, timetable_size: dict) -> None
 
 async def check_shedule():
     while True:
-        logging.info("check shedule")
         hour_biysk = datetime.now(TIME_ZONE).strftime('%H%M%S')
         if int(hour_biysk) in range(100000, 200000):
+            logging.info("check shedule")
             [await comparison_size_bgc(i, timetable_old_size) for i in range(1, 5)]
             await asyncio.sleep(180)
         else:
-            logging.info("check shedule - no update")
+            logging.info("check shedule - time > 20:00")
             await asyncio.sleep(600)
