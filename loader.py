6ea5cf7717dbb649decad9cfb73c205769bfafcd
Fallern <alesha.loginov2000@gmail.com>
2022-04-06 12:40:46 +0300

    feat: add redisStorage

--- loader.py
+++ loader.py
@@ -1,10 +1,13 @@
 from aiogram import Bot, types, Dispatcher
 import redis
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from data import config
 
 bot = Bot(token=config.API_TOKEN_TELEGRAM, parse_mode=types.ParseMode.HTML)
 
-dp = Dispatcher(bot)
+storage = RedisStorage2(state_ttl=20, data_ttl=20)
+
+dp = Dispatcher(bot, storage=storage)
 
 redis_client = redis.Redis(decode_responses=True)
