6eee2099245af2f8c5eda1b51d3afa1aaf0f018c
Fallern <alesha.loginov2000@gmail.com>
2022-04-06 06:36:20 +0300

    feat: add connect Redis

--- loader.py
+++ loader.py
@@ -1,7 +1,10 @@
 from aiogram import Bot, types, Dispatcher
+import redis
 
 from data import config
 
 bot = Bot(token=config.API_TOKEN_TELEGRAM, parse_mode=types.ParseMode.HTML)
 
 dp = Dispatcher(bot)
+
+redis_client = redis.Redis(decode_responses=True)
