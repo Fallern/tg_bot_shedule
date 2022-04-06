06e48eb9e395b038c18f5c0fa6a4b419eab4f4e4
Fallern <alesha.loginov2000@gmail.com>
2022-04-06 12:43:03 +0300

    feat: add model UserTg

--- database/models.py
+++ database/models.py
@@ -12,3 +12,16 @@ class GroupsImg(Model):
 
     def __str__(self):
         return self.name
+
+
+class UserTg(Model):
+    id = fields.CharField(max_length=200, pk=True)
+    name = fields.CharField(max_length=60)
+    college_building = fields.IntField()
+    group = fields.ForeignKeyField("models.GroupsImg")
+
+    class Meta:
+        table = "users_tg"
+
+    def __str__(self):
+        return self.id
