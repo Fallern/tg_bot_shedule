2da7089e7f2d2e13b173a6c454ab72793c9de370
Fallern <alesha.loginov2000@gmail.com>
2022-04-08 03:25:06 +0300

    refactor: change default bot commands

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,5 +4,8 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
         types.BotCommand("start", 'Запустить бота'),
-        types.BotCommand('help', 'Помощь')
+        types.BotCommand('help', 'Помощь'),
+        types.BotCommand('register', 'Рассылка расписания'),
+        types.BotCommand('about', 'Информация о проекте')
+
     ])
