818147732b8339a3dc6f892be79e10966452c9fb
Fallern <alesha.loginov2000@gmail.com>
2022-04-08 03:24:03 +0300

    feat: add FSM registerState

--- states/register_state.py
+++ states/register_state.py
@@ -4,5 +4,6 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class RegisterState(StatesGroup):
     name = State()
     college_number = State()
+    page_data = State()
     group = State()
 
